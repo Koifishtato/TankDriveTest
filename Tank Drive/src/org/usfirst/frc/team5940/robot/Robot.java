327b6d6042ef224b02cb4c2b3f0765f0dd012c2e
Koifishtato <aloeffler20@dtechhs.org>
2016-12-15 15:25:11 -0800

    Added comments

--- Tank Drive/src/org/usfirst/frc/team5940/robot/Robot.java
+++ Tank Drive/src/org/usfirst/frc/team5940/robot/Robot.java
@@ -1,5 +1,6 @@
 package org.usfirst.frc.team5940.robot;
 import java.lang.Math;
+import java.util.Random;
 import edu.wpi.first.wpilibj.DigitalInput;
 
 import edu.wpi.first.wpilibj.SampleRobot;
@@ -38,35 +39,70 @@ public class Robot extends SampleRobot {
 	/**
 	 * Runs the motors with tank steering.
 	 */
+	//This tells the code "This is a ball sensor on port 9"
 	public static DigitalInput ballDetector = new DigitalInput(9);
+	
+	//This is the Operator Control code. This runs when we start the robot.
 	public void operatorControl() {
+		//This tells the code to make a motor group called Motors. This is where the wheel motors are stored.
 		MotorGroup Motors = new MotorGroup();
+		
+		//This is just for Noah...
+		Random rand = new Random();
+		
+		
+		//This tells the code there is a Motor to be called Roller on port 0.
 		MotorGroup Roller = new MotorGroup(0);
+		
+		//This tells the code there is a joystick on port 0.
 		Joy Joys = new Joy(0);
+		
+		//This makes a timer...
+		Timer time = new Timer();
+		
+		//This makes an array of numbers to store the joystick positions.
 		double[] pos;
+		
+		//This is a loop that runs the code inside the Braces until the code is disabled.
 		while (isOperatorControl() && isEnabled()) {
+			
+			//This makes the array of numbers we made eariler be set to the positions of the joysticks.
 			pos = Joys.get(0, 6);
+			
+			//Prints an random int
+			SmartDashboard.putNumber("Random",rand.nextInt(1000));
+			
+			//Checks if the left or right sticks are up or down
 			if(Math.abs(pos[1])>0.1||Math.abs(pos[5])>0.1){
+				//This tells the drive motors to go at the inverse of the speed of the joysticks.
 				Motors.goL(-(pos[1]));
 				Motors.goR(-(pos[5]));
 			}
 			else{
+				//This tells the motors to stop if there is no joystick input.
 				Motors.goL(0);
 				Motors.goR(0);
 			}
+			//This checks if the trigger is pushed down.
 			if(pos[3]>0.4){
+				//This tells the roller motors to go at half the speed of the trigger.
 				Roller.go(-pos[3]/2, 0);
-			}
+			} 
+			
+			//This checks if the trigger is pressed, and the ball detector is not activated.
 			else if(pos[2]>0.4 & getCorrectedDetector()){
 				Roller.go(pos[2]/2, 0);
 			}
+			//This stops the Roller.
 			else{
 				Roller.go(0, 0);
 			}
+			//This just puts the value of the sensor on the dashboard
 			SmartDashboard.putBoolean("Sensor", getCorrectedDetector());
+			Time.sleep(100);
 		}
 		
-		
+		//This is not code that is in use right now.
 		/**this.camera = new Thread(new Camera(this));
 		MotorGroup Motors = new MotorGroup();
 		Joy Joys = new Joy(0);
